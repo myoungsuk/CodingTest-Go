f7dab06dc6049eca9c2682d21f9cc63c09887010
강명석 <k4858678@gmail.com>
2023-10-13 14:40:37 +0900

    10/13

--- main/main.go
+++ main/main.go
@@ -2,28 +2,58 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	n := 10
-	fmt.Print(solution(n))
+	a := 2
+	b := 6
+	c := 1
+
+	fmt.Print(solution(a, b, c))
 }
 
-func solution(n int) int {
-	answer := 0
+func solution(a int, b int, c int) int {
 
-	if n%2 == 1 {
-		for i := 0; i <= n; i++ {
-			if i%2 == 1 {
-				answer += i
-			}
-		}
-	} else {
-		for i := 1; i <= n; i++ {
-			if i%2 == 0 {
-				answer += i * i
-			}
-		}
+	original := []int{a, b, c}
+	arr := []int{a, b, c}
+	count := GetMaxSameCount([]int{a, b, c})
+	ret := 1
+
+	for i := 0; i < count; i++ {
+		ret *= Sum(arr)
+		arr = Multiply(arr, original)
 	}
-	return answer
+
+	return ret
+}
+
+func Multiply(a []int, b []int) []int {
+	var ret []int
+	for i := range a {
+		ret = append(ret, a[i]*b[i])
+	}
+	return ret
+}
+
+func GetMaxSameCount(arr []int) int {
+	sort.Ints(arr)
+	prev := arr[0]
+	ret := 1
+
+	for i := 1; i < len(arr); i++ {
+		if prev == arr[i] {
+			ret++
+		}
+		prev = arr[i]
+	}
+	return ret
+}
+
+func Sum(arr []int) int {
+	ret := 0
+	for _, item := range arr {
+		ret += item
+	}
+	return ret
 }
