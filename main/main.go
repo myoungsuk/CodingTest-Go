98573a9538c70c934dba26d738c103223c9b938e
강명석 <k4858678@gmail.com>
2023-10-14 23:22:57 +0900

    10/14

--- main/main.go
+++ main/main.go
@@ -2,58 +2,26 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	a := 2
-	b := 6
-	c := 1
-
-	fmt.Print(solution(a, b, c))
+	num_list := []int{3, 4, 5, 2, 1}
+	fmt.Print(solution(num_list))
 }
 
-func solution(a int, b int, c int) int {
+func solution(num_list []int) int {
 
-	original := []int{a, b, c}
-	arr := []int{a, b, c}
-	count := GetMaxSameCount([]int{a, b, c})
-	ret := 1
+	sq := 1
+	sum := 0
 
-	for i := 0; i < count; i++ {
-		ret *= Sum(arr)
-		arr = Multiply(arr, original)
+	for _, num := range num_list {
+		sq *= num
+		sum += num
 	}
 
-	return ret
-}
-
-func Multiply(a []int, b []int) []int {
-	var ret []int
-	for i := range a {
-		ret = append(ret, a[i]*b[i])
+	if sq < sum*sum {
+		return 1
 	}
-	return ret
-}
 
-func GetMaxSameCount(arr []int) int {
-	sort.Ints(arr)
-	prev := arr[0]
-	ret := 1
-
-	for i := 1; i < len(arr); i++ {
-		if prev == arr[i] {
-			ret++
-		}
-		prev = arr[i]
-	}
-	return ret
-}
-
-func Sum(arr []int) int {
-	ret := 0
-	for _, item := range arr {
-		ret += item
-	}
-	return ret
+	return 0
 }
